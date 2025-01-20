1f98414397cc5983559b94fe141220015c547d2a
Shaketo <125228665+Shaketo@users.noreply.github.com>
2025-01-20 12:25:56 +0900

    Added character count

--- widgets/memo_editor.dart
+++ widgets/memo_editor.dart
@@ -1,55 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:memo_app/controller/memo_handler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memo_app/providers/new_id_notifier.dart';
 
-class EditHandling extends ConsumerStatefulWidget {
-  const EditHandling({super.key, this.id, this.memo});
+class MemoEditor extends ConsumerStatefulWidget {
+  MemoEditor(
+      {super.key, this.id, this.memo, required this.memoHandler, this.newId});
 
   final int? id;
   final String? memo;
+  final MemoHandler memoHandler;
+  dynamic newId;
 
   @override
-  ConsumerState<EditHandling> createState() {
+  ConsumerState<MemoEditor> createState() {
     return _EditHandlingState();
   }
 }
 
-class _EditHandlingState extends ConsumerState<EditHandling> {
-  final memoHandler = MemoHandler();
+class _EditHandlingState extends ConsumerState<MemoEditor> {
+  int? characterCounter;
 
   @override
   void initState() {
     if (widget.id != null) {
-      memoHandler.memoController.text = widget.memo!;
+      widget.memoHandler.memoController.text = widget.memo!;
     }
+    characterCounter = widget.memoHandler.memoController.text.length;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(24),
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 5),
       child: Column(
         children: [
-          TextField(
-            controller: memoHandler.memoController,
-            keyboardType: TextInputType.text,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  if (widget.id != null) {
-                    memoHandler.updateMemo(widget.id!);
-                  } else {
-                    memoHandler.saveMemo();
-                  }
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Text('文字数：$characterCounter'),
+          ]),
+          SingleChildScrollView(
+            child: Expanded(
+              child: TextField(
+                controller: widget.memoHandler.memoController,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: const InputDecoration(border: InputBorder.none),
+                autofocus: true,
+                onChanged: (value) {
+                  setState(() {
+                    characterCounter = value.length;
+                  });
                 },
-                child: const Icon(Icons.save),
               ),
-            ],
-          )
+            ),
+          ),
         ],
       ),
     );
